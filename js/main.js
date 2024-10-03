f714111387fb5d33517d853793a5d8cad400fbe0
DanielleSchell <danielle.schell@gmail.com>
2024-10-02 22:29:26 -0600

    completed

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 // Enter your code below.
+// step 1
+const divResources = document.querySelector('.javascript-resources');
+const buttonShowResources = document.querySelector('#show-resources');
+
+buttonShowResources.addEventListener('click', () => {
+    divResources.classList.remove('d-none');
+});
+
+// step 2
+divResources.addEventListener('mouseover', (event) => {
+    console.log(event.target);
+    event.target.classList.add('fw-bold');
+});
+
+// step 3
+divResources.addEventListener('mouseout', (event) => {
+    event.target.classList.remove('fw-bold');
+});
+
+// step 4
+divResources.addEventListener('click', (event) => {
+    event.target.classList.add('fst-italic');
+});
